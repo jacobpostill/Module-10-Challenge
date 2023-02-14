8686a5fb59b036ee05d391a441942bc853dcebe0
Jacob Postill <postilljacob@gmail.com>
2023-02-14 12:57:01 -0500

    added comments

--- index.js
+++ index.js
@@ -2,16 +2,8 @@ const inquirer = require('inquirer');
 const  {Square, Triangle, Circle, GenerateLogo} = require('./lib/shapes')
 const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt');
 inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt)
+// In these lines of codes we are importing previous classes to be used in this Javascript
 
-
-class logoInfo {
-    constructor(text, textCol, shape, shapeCol) {
-        this.text = text
-        this.textCol = textCol
-        this.shape = shape
-        this.shapeCol = shapeCol
-    }
-}
 
 function info() {
     inquirer
@@ -21,6 +13,7 @@ function info() {
             message: 'Which letters would you like in your logo? (Up to three)',
             name: 'text',
             maxLength: 3
+            //We import maxlength-input and use it here so no one will be able to create a logo with more then three characters
         },
         {
             type: 'list',
@@ -38,12 +31,15 @@ function info() {
             message: 'What color would you like the shape to be? (Enter name or hex key - include #)',
             name: 'shapeCol'
         }
+        //Here we are creating a function to ask the prompts oif what they would like for there logo
     ])
     .then((response) => {
         console.log(response)
         let generateLogo = new GenerateLogo(response.text, response.textCol, response.shape, response.shapeCol)
         generateLogo.shapeChoice()
+        //Here we are using an imported GenerateLogo and asiging the constrocturs the values from the prompt. Then we are running the shape choice function to create the logo
     })
 }
 
 info()
+//Here we run the info function to activate the prompts
